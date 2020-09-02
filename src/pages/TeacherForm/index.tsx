b46bcb59bc14ac3b3087fff0b90c66d557d3dd00
Francelino <fmarcilio1989@gmail.com>
2020-09-02 20:24:19 -0300

    estilo de pagina

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/input';
+
+import './styles.css';
 
 function TeacherForm() {
     return (
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Incríveil que você quer dar aulas"/>
+            <PageHeader 
+                title="Incríveil que você quer dar aulas"
+                description="O primeiro passo é preencher o formulario de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome Completo"/>
+                    <Input name="avatar" label="Avatar"/>
+                    <Input name="whatsapp" label="WhatsApp"/> 
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a Aula</legend>
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora aula"/>
+                </fieldset>
+            </main>
         </div>
     )
 }
