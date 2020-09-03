a605bcc06bf99e8c0224d704482e5806b22fed3c
Francelino <fmarcilio1989@gmail.com>
2020-09-03 14:59:11 -0300

    estilo da pagina de cadastro de proffys

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import Input from '../../components/input';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
+
 
 function TeacherForm() {
     return (
@@ -18,13 +23,25 @@ function TeacherForm() {
                     <Input name="name" label="Nome Completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="WhatsApp"/> 
+                    <Textarea name="bio" label="Biografia"/>
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a Aula</legend>
-                    <Input name="subject" label="Matéria"/>
+                    <Select name="subject" label="Matéria"/>
                     <Input name="cost" label="Custo da sua hora aula"/>
                 </fieldset>
+
+                <footer>
+                <p>
+                    <img src={warningIcon} alt="Aviso importante" />
+                    Importante! <br />
+                    Preencha todos os dados
+                </p>
+                <button type="button">
+                    Salvar Cadastro
+                </button>
+            </footer>
             </main>
         </div>
     )
